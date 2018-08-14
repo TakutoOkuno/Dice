66084b5dbf399b637b916ae7bddecf7c812928ef
Takuto Okuno <38966433+TakutoOkuno@users.noreply.github.com>
2018-08-15 00:53:22 +0900

    100、面ダイスを1〜100面ダイスに変更

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-document.getElementById('btn1').onclick = function(){
-dice = Math.floor(Math.random()*100)+1;
-document.querySelector('#test1').innerHTML = dice;
+window.onload = function createSelectBox(){
+  for(var i=1;i<=100;i++){
+    let op = document.createElement("option");
+    op.value = i;
+    op.text = i;
+document.getElementById("sel").appendChild(op);
+  }
 };
+
+document.getElementById('btn').onclick = function createResult(){
+dice = Math.floor(Math.random()*n)+1;
+document.querySelector('#test').innerHTML = dice;
+};
+
+document.getElementById('numBtn').onclick = function(){
+n = document.getElementById('sel').value;
+}
